0f8ccbe8b6161716587277e8821858fd355d3ca5
hafees <mohammedhafees1021@gmail.com>
2024-02-23 18:15:05 +0530

    fixing the flash file.

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Flashing script
-
+set -x
 ARDUINO_CLI=$HOME/bin/arduino-cli
 BOARD_REGEX='[[:alnum:]]\+:[[:alnum:]]\+:[-_[:alnum:]]\+'
-BOARD=$($ARDUINO_CLI board list | grep -o  $BOARD_REGEX)
-PORT="/dev/ttyACM0"
+BOARD=$($ARDUINO_CLI board list | grep -o  $BOARD_REGEX) 
+
+#ESP32 board not detected automatically So we give manual board name for esp32
+if [ "$BOARD" = "" ]; then
+	BOARD=esp32:esp32:esp32
+fi
+PORT="/dev/ttyUSB0"
 
 #Install Dependent libraries
 $ARDUINO_CLI lib install pubsubclient &&
